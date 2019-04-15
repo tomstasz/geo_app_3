1f1295c23ceb3239d20f77305c26e8cb79c74288
Tomasz Staszkiewicz <tomsta.kreacja@gmail.com>
2019-04-15 02:54:20 +0200

    create basic APIView and urls

--- geo_3/views.py
+++ geo_3/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.gis.geos import fromstr
+from django.contrib.gis.measure import D
+from django.contrib.gis.db.models.functions import Distance
+from rest_framework.generics import ListCreateAPIView
 
-# Create your views here.
+from geo_3.models import Location
+from geo_3.serializers import LocationSerializer
+
+
+class LocationList(ListCreateAPIView):
+    """Display locations or add a new one"""
+    serializer_class = LocationSerializer
+    queryset = Location.objects.all()
--- main/urls.py
+++ main/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from geo_3.views import LocationList
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('locations/', LocationList.as_view(), name='locations'),
 ]
