7d9891a3eaff82e4ac4bfe5bba89dd018ab9a31f
Tomasz Staszkiewicz <tomsta.kreacja@gmail.com>
2019-04-15 03:15:08 +0200

    override get_queryset method

--- geo_3/views.py
+++ geo_3/views.py
@@ -11,6 +11,40 @@ class LocationList(ListCreateAPIView):
     """Display locations or add a new one"""
     serializer_class = LocationSerializer
 
+    def get_queryset(self):
+        """Find closest location or search given perimeter"""
+        longitude = self.request.query_params.get('user_longitude')
+        latitude = self.request.query_params.get('user_latitude')
+        perimeter = self.request.query_params.get('perimeter')
+        print('longitude: ', longitude)
+        print('latitude: ', latitude)
+        print('perimeter: ', perimeter)
+        queryset = self.queryset
+        if longitude and latitude and perimeter:
+            try:
+                user_pnt = fromstr(
+                    'POINT({} {})'.format(
+                        longitude, latitude), srid=4326
+                )
+                queryset = Location.objects.filter(
+                    point__distance_lte=(user_pnt, D(km=abs(int(perimeter)))))
+            except ValueError:
+                print('Error, please enter numbers')
+            print('queryset: ', queryset)
+        elif longitude and latitude:
+            try:
+                user_pnt = fromstr(
+                    'POINT({} {})'.format(
+                        longitude, latitude), srid=4326
+                )
+                queryset = Location.objects.annotate(
+                    distance=Distance(
+                        user_pnt, 'point')).order_by('distance')[:1]
+            except ValueError:
+                print('Error, please enter numbers')
+            print('queryset: ', queryset)
+        return queryset
+
     def perform_create(self, serializer):
         """Create a new Location object"""
         longitude = serializer.validated_data['longitude']
